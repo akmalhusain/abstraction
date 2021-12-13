f074c92ad7613613d92dfe8f733389c864aca749
akmalhusain <70431817+akmalhusain@users.noreply.github.com>
2021-12-13 11:32:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace abstraction
+{
+    abstract class animal
+    {
+        public abstract void animalsound(); //abstract method with out body
+        public void sleep() // regular method
+        {
+            Console.WriteLine("sleep sound is zzz");
+        }
+    }
+    class dog : animal
+    {
+        public override void animalsound()
+        { 
+            Console.WriteLine("the dog say:bow,bow");
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            dog obj1 = new dog();
+            obj1.animalsound();
+            obj1.sleep();
+            
+        }
+    }
+}
